ef343a67f4823eb1e44c120a8c72a824f6a164a9
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-02 11:17:12 +0300

    even or odd
    internship

---
+++ EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              
+            int number1;
+            Console.WriteLine("Enter a number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number1 % 2 == 0)
+            {
+                Console.WriteLine("The number is an even number..... ");
+            }
+            else
+            {
+                Console.WriteLine("The number is an odd number...");
+            }
+
+        }
+
+    }
+
+}
